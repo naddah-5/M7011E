66e3e41d289d5aba9cd7cd8ed8d69f3db6fcbb80
naddah-5 <andersoskardahl@hotmail.com>
2022-11-21 11:29:04 +0100

    working on more tests

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,12 @@ class Question(models.Model):
     def __str__(self):
             return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        """
+        Check if the publish date for a qusetion is from within the last day.
+        """
+        now = timezone.now()
+        old = now - datetime.timedelta(days=1)
+        return old <= self.pub_date <= now
 
 
 class Choice(models.Model):
