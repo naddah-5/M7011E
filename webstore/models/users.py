d13d87cc98ceb45e23991c0e8697464e57cbb90a
naddah-5 <andersoskardahl@hotmail.com>
2022-11-30 08:57:04 +0100

    reversed user->password FK relationship

--- webstore/models/users.py
+++ webstore/models/users.py
@@ -1,5 +1,4 @@
 from django.db import models
-from .passwords import Passwords
 from typing import Literal
 
 class Users(models.Model):
@@ -14,7 +13,6 @@ class Users(models.Model):
     ]
     email: models.EmailField = models.EmailField()
     create_time: models.DateTimeField = models.DateTimeField()
-    password: models.ForeignKey = models.ForeignKey(Passwords, on_delete=models.CASCADE)
     role: models.CharField = models.CharField(
         max_length=2,
         choices=ROLE,
