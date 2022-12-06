1634205b66d729e5a7f761108501512ae05e09e0
naddah-5 <andersoskardahl@hotmail.com>
2022-12-06 10:41:27 +0100

    removed old structure and fixed test

--- webstore/models/tests/tests_users.py
+++ webstore/models/tests/tests_users.py
@@ -1,32 +1,30 @@
 from django.test import TestCase
-from django.db import models
+from django.db import IntegrityError, models
 from webstore.models import Users
-from webstore.models.queries import create_user as db
-
 import datetime
 
 class UsersTests(TestCase):
 
     def test_create_user_email(self) -> None:
-        db.create_new_user(
-            user_email="testuser@hotmail.com", 
-            given_create_time=datetime.datetime.now(), 
-            set_role="CM"
+        Users.objects.create(
+            email="testuser@hotmail.com", 
+            create_time=datetime.datetime.now(), 
+            role="CM"
         )
         created_user: Users = Users.objects.get(email = "testuser@hotmail.com")
         self.assertEqual(created_user.email, "testuser@hotmail.com")
 
     def test_create_user_unique(self) -> None:
         
-        _: bool = db.create_new_user(
-            user_email="testuser@hotmail.com",
-            given_create_time=datetime.datetime.now(),
-            set_role="CM"
+        _: Users = Users.objects.create(
+            email="testuser@hotmail.com",
+            create_time=datetime.datetime.now(),
+            role="CM"
         )
-        same_user: bool = db.create_new_user(
-            user_email="testuser@hotmail.com",
-            given_create_time=datetime.datetime.now(),
-            set_role="CM"
-        )
+        with self.assertRaises(IntegrityError):
+            same_user: Users = Users.objects.create(
+                email="testuser@hotmail.com",
+                create_time=datetime.datetime.now(),
+                role="CM"
+            )
         
-        self.assertFalse(same_user)
