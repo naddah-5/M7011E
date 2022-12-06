062b593b275b6fded4abd2a6b9441b61f03dcf2b
Zoupi1 <zoupi1111@hotmail.com>
2022-12-06 05:20:18 +0100

    beginning of login and register

--- webstore/urls.py
+++ webstore/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import home, about_us
+from .views import home, about_us, login, register
 
 app_name = 'webstore'
 
 urlpatterns = [
     path('', home, name = 'home'),
     path('about-us', about_us, name = 'about_us'),
+    path('login', login, name = 'login'),
+    path('register', register, name = 'register'),
 ]
