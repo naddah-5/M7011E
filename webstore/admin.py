9bd2ae7469067560e0a16d79b64ff8d490f9a3c6
Zoupi1 <zoupi1111@hotmail.com>
2022-12-29 05:29:09 +0100

    Admin Page and Superuser/Admin

--- webstore/admin.py
+++ webstore/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Product
+from django import forms
+from django.contrib.auth.models import Permission
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+
+from webstore.models import UserProfile, Product, Order
 
 # Register your models here.
+admin.site.register(Permission)
+admin.site.unregister(get_user_model())
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [
+        UserProfileInline,
+    ]
+
+# Super user
+admin.site.register(get_user_model(), UserAdmin)
+
+# Admin
+# Super User needs to set permission for Admin to view/edit these models
+# TODO: Add all the models
+admin.site.register(Product)
+admin.site.register(Order)
